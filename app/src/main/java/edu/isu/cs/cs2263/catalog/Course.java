d6cba27b8d2d6aec00599d38d8b4a2bad415bde3
Brandon Watkins <Brandon@Watkins.blue>
2021-01-26 23:51:23 -0700

    Finished the assignment with the addition of my buttons, event handlers, course list output.

--- app/src/main/java/edu/isu/cs/cs2263/catalog/Course.java
+++ app/src/main/java/edu/isu/cs/cs2263/catalog/Course.java
@@ -1,8 +1,13 @@
+/**
+ * Course class, utilizing a static array of departments, as per my assignment instructions.
+ *
+ * @author Brandon Watkins
+ * @version 2.0.0
+ * @since 2021-01-20
+ */
 package edu.isu.cs.cs2263.catalog;
 
-import java.util.List;
-
-public class Course {
+public class Course implements Comparable<Course> {
     private static final Department[] DEPARTMENTS = {
             new Department("Computer Science", "CS"),
             new Department("Mathematics", "MATH"),
@@ -23,6 +28,13 @@ public class Course {
     public Department getDepartment() { return department; }
     public static Department[] getDepartments() { return DEPARTMENTS; }
 
+    /**
+     *
+     * @param courseNumber String representation of the course's "number" (ie. CS *2263*).
+     * @param courseName The course's name (ie. *Advanced Object-Oriented Programming*).
+     * @param courseCredits The number of credits the course is worth (ie. *3*).
+     * @param departmentIndex The index of the courses department in the departments array.
+     */
     public Course(String courseNumber, String courseName, int courseCredits, int departmentIndex) {
         number = courseNumber;
         name = courseName;
@@ -30,5 +42,14 @@ public class Course {
         department = DEPARTMENTS[departmentIndex];
     }
 
-    public String toString() { return number + " " + name; }
+    /**
+     * The string representation of the object.
+     * @return String (CS  2263  Advanced Object-Oriented Programming  (3 cr))
+     */
+    public String toString() { return getDepartment().getCode() + "  " + number + "  " + name + "  (" + credits + " cr)"; }
+
+    @Override
+    public int compareTo(Course c) {
+        return (getDepartment().getCode() + " " + number).compareTo(c.getDepartment().getCode() + " " + c.getNumber());
+    }
 }
